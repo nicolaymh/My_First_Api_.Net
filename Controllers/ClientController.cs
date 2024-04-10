6fac655108671db31d3cd4e701b3831c5f6743c1
Nicolay <nicolaymorenoherrera@gmail.com>
2024-04-10 18:19:05 -0500

    Agregar archivos de proyecto.

---
+++ Controllers/ClientController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+using My_First_Api_.Net.Models;
+
+namespace My_First_Api_.Net.Controllers
+{
+    [ApiController]
+    [Route("/client")]
+    public class ClientController : ControllerBase
+    {
+        private readonly IMemoryCache _cache;
+        private const string ClientCacheKey = "clientList";
+
+        public ClientController(IMemoryCache cache)
+        {
+            _cache = cache;
+            Console.WriteLine(_cache);
+
+            _cache.CreateEntry(ClientCacheKey).Value ??= new List<Client>();
+            Console.WriteLine(_cache);
+        }
+
+
+    }
+}
---
+++ Models/Client.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace My_First_Api_.Net.Models
+{
+    public class Client
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+    }
+}
